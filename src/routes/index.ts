f1741e6d5251140275f1bfcf801e687a1c1a065c
Renato Bittencourt <renato.newpremium@gmail.com>
2020-12-12 15:51:12 -0300

    first commit

---
+++ src/routes/index.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+
+  // response.json({ message: 'Hello GoStack' });
+});
+
+export default routes;
